3e6e87307f020be5b37ef6e2b516db7f2413da8d
alexhernandezord <44363090+alexhernandezord@users.noreply.github.com>
2022-11-17 15:38:09 +0100

    feat: [sc-101103] Documentación plugins (#11)
    * plugins sidebar and base docs
    
    * plugins install and basic config
    
    * plugins widget and more configs
    
    * more plugins
    
    * english translate and various fixes
    
    * change warnings for cautions and some infos

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,5 +6,5 @@ export default function Home() {
   const { i18n } = useDocusaurusContext();
   const prefix = i18n.currentLocale === i18n.defaultLocale ? '' : '/' + i18n.currentLocale
 
-  return <Redirect to={ prefix +  "/docs/checkout-process/" } />;
+  return <Redirect to={ prefix +  "/docs/api/checkout-process/" } />;
 }
